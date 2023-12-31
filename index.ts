e7ab5674c561da7fe25204c2a2323ebcc16f806a
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-12-31 20:49:33 +0530

    rate limit

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routes/route";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
@@ -15,12 +16,20 @@ app.use(
 dotenv.config();
 app.use(express.json());
 
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 15 minutes
+  max: 10, // 100 requests per window
+  message: "Too many requests, please try again later.",
+});
+if (process.env.RATE_LIMIT === "true") {
+  app.use(limiter);
+}
 app.use("/api/v1", router);
 app.get("/", (req, res) => {
   res.send("its ok");
 });
 
-const Port = process.env.PORT || 5000;
+const Port = process.env.PORT || 5001;
 app.listen(Port, () => {
   console.log(`Server running on port ${Port}`);
 });
